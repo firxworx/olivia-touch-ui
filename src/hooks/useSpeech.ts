210e19f5307fcb16ae14dfa0b41dba35c6ceb51e
Kevin Firko <kfirko@bitcurve.com>
2021-08-07 00:20:13 -0400

    move useSpeech hook

--- src/hooks/useSpeech.ts
+++ src/hooks/useSpeech.ts
@@ -1,6 +1,12 @@
 import React from 'react'
 import { SpeechContext } from './SpeechContext'
 
+/**
+ * React hook that provides an interface to the HTML5 text-to-speech API.
+ * Components that use this hook must be wrapped in the corresponding context provider.
+ *
+ * @see SpeechContextProvider
+ */
 export const useSpeech = () => {
   const context = React.useContext(SpeechContext)
 
