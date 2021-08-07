3097013a9b74cee1d47cc366881d37989582db9f
Kevin Firko <kfirko@bitcurve.com>
2021-08-07 00:24:45 -0400

    wrap _app in InteractionSuppressor component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from 'next/app'
 import '../styles/tailwind.css'
 import { SpeechContextProvider } from '../hooks/speech/SpeechContext'
+import { InteractionSuppressor } from '../components/layout/InteractionSuppressor'
 
 function ProjectApp({ Component, pageProps }: AppProps) {
   return (
     <SpeechContextProvider>
-      <Component {...pageProps} />
+      <InteractionSuppressor>
+        <Component {...pageProps} />
+      </InteractionSuppressor>
     </SpeechContextProvider>
   )
 }
