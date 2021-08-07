6629c552f49ba8b098a46035eb45644cae0ea190
Kevin Firko <kfirko@bitcurve.com>
2021-08-07 12:02:18 -0400

    add TouchGameScreen to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import { useTransition, animated } from '@react-spring/web'
-import { useSpeech } from '../hooks/useSpeech'
+import { useSpeech } from '@/hooks/useSpeech'
 
-import { GridLayout } from '../components/layout/GridLayout'
-import { SpeechScreen } from '../screens/SpeechScreen'
+import { GridLayout } from '@/layout/GridLayout'
+import { SpeechScreen } from '@/screens/SpeechScreen'
+import { TouchGameScreen } from '@/screens/TouchGameScreen'
 
-const screens = [{ name: 'Three Fly Mode', component: SpeechScreen }]
+const screens = [
+  { name: 'Speech Screen', component: SpeechScreen },
+  { name: 'Button Game', component: TouchGameScreen },
+]
 
 const Mode: React.FC<{ index: number }> = ({ index }) => {
   return screens[index].component({})
@@ -31,7 +35,6 @@ export default function IndexPage() {
     },
     enter: { opacity: 1, transform: 'translate3d(0%, 0px, 0px)' },
     leave: { opacity: 0, position: 'absolute', transform: 'translate3d(100%, 0px, 0px)' },
-    // delay...
     // onRest: () => ...
   })
 
