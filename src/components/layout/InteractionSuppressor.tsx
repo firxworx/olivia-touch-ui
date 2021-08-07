bdf1e2615bd204da160efb76d8fcf8223e1575a4
Kevin Firko <kfirko@bitcurve.com>
2021-08-07 00:21:48 -0400

    implement InteractionSuppressor component for high-level touchAction + use the useSuppress.. hooks

---
+++ src/components/layout/InteractionSuppressor.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { useSuppressContextMenu } from '@/hooks/useSuppressContextMenu'
+import { useSuppressMultiTouch } from '@/hooks/useSuppressMultiTouch'
+
+/**
+ * Utility component for suppressing the right-click/long-press context menu,
+ * multi-touch gesures, and touch actions (specifically pinch zoom on touch screens).
+ */
+export const InteractionSuppressor: React.FC<{ suppressScrolling?: boolean }> = ({
+  children,
+  suppressScrolling = false,
+}) => {
+  useSuppressContextMenu()
+  useSuppressMultiTouch()
+
+  return <div style={{ touchAction: suppressScrolling ? 'none' : 'pan-x pan-y' }}>{children}</div>
+}
